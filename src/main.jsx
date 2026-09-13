4e6350b9035bf5b5204c70a02b01b296553fc200
hareezahamed888-ux <hareezahamed888@gmail.com>
2026-09-13 21:03:32 +0530

    Add Vite + React scaffolding for Vercel deployment (no changes to app UI/logic)

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import JPRBusApp from '../JPRBusApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <JPRBusApp />
+  </React.StrictMode>,
+)
